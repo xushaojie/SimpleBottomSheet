cb56c16b43810bdd84fa7ccd9c7f449a3dab7c88
xushaojie <xsjayz@163.com>
2016-04-18 11:19:58 +0800

    first commit code

--- app/src/main/java/com/shaojie/demo/simplebottomsheet/MainActivity.java
+++ app/src/main/java/com/shaojie/demo/simplebottomsheet/MainActivity.java
@@ -2,6 +2,7 @@ package com.shaojie.demo.simplebottomsheet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.bs_show_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TestBottomSheet bottomSheet = new TestBottomSheet(MainActivity.this);
+                bottomSheet.show();
+            }
+        });
     }
+
 }
